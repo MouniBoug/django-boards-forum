1d7d1c8bc4e2a80d87155aed30190e403dee91c6
Mounir Inyock <inyockmounir@gmail.com>
2018-04-21 17:06:30 +0200

    Correct the dj database importing

--- Web_Board_Project/settings.py
+++ Web_Board_Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 from decouple import config, Csv  # have to install python-decouple package
-import dj_databse_url
+import dj_database_url
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
